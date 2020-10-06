6ed2c738203641caec1aaf5cc9bb528e243e7c40
ahmadsalimi <ahsa9978@gmail.com>
2020-10-06 17:18:22 +0330

    change style

--- client/assets/scripts/main.js
+++ client/assets/scripts/main.js
@@ -1,3 +1,33 @@
+$(document).ready(() => {
+    $('#nav-brand').hover(() => {
+        $('#dev')
+            .removeClass('bg-warning')
+            .addClass('bg-transparent')
+            .removeClass('text-dark')
+            .addClass('text-white');
+        $('#ama')
+            .addClass('bg-warning')
+            .removeClass('bg-transparent')
+            .addClass('text-dark')
+            .removeClass('text-white');
+
+        $('#nav-icon').addClass('text-warning');
+    }, () => {
+        $('#ama')
+            .removeClass('bg-warning')
+            .addClass('bg-transparent')
+            .removeClass('text-dark')
+            .addClass('text-white');
+        $('#dev')
+            .addClass('bg-warning')
+            .removeClass('bg-transparent')
+            .addClass('text-dark')
+            .removeClass('text-white');
+
+        $('#nav-icon').removeClass('text-warning')
+    });
+});
+
 function showMessage(title, message) {
     $('#response-title').html(title);
     $('#response-message').html(message);
