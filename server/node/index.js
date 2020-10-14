e32a1c9d5c9a2b0e8996626612e95c60050aabce
ahmadsalimi <ahsa9978@gmail.com>
2020-10-15 00:09:55 +0330

    a small change

--- server/node/index.js
+++ server/node/index.js
@@ -1,7 +1,9 @@
 import express from "express";
+
 const app = express();
-const port = 3000;
+
 const hostname = "0.0.0.0"
+const port = 3000;
 
 app.get("/helloworld/node", (_, res) => {
 	res.send("Hello World!!")
